4f11fac176f4d8531aa83e7765de3b7fce0147ff
Bruno Domingues <brunood07@gmail.com>
2022-06-05 19:37:14 -0300

    feat: sidebar and post created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { Header } from "./components/Header";
 import { Post } from "./components/Post";
+import { Sidebar } from "./components/Sidebar";
 
 import styles from "./App.module.css";
+
 import "./styles/global.css";
-import { Sidebar } from "./components/Sidebar";
 
 export default function App() {
   return (
@@ -13,11 +14,8 @@ export default function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Bruno Domingues"
-            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum libero suscipit rem sunt, excepturi itaque dolorem laudantium vero soluta veniam quidem enim, aliquam qui adipisci saepe, iste ducimus. Aliquam, inventore."
-          />
-          <Post author="Teste" content="Um novo post" />
+          <Post />
+          <Post />
         </main>
       </div>
     </>
